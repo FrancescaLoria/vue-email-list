69f1c29fba0015cbd156a6077c8f03655e11cc62
FrancescaLoria <francesca.loria90@hotmail.it>
2023-04-11 16:46:06 +0200

    completed email

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      text: "ciao",
+      myEmailList: [],
     };
   },
   beforeMount() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((resp) => {
-        console.log(resp.data.response);
-      });
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((resp) => {
+          this.myEmailList.push(resp.data.response);
+        });
+    }
   },
 }).mount("#app");
